cc9aeef8f160ab87fca3920824ee69b958b1bbf9
Duri <durid@union.edu>
2014-08-02 21:22:03 -0400

    Add Menu View Controller

---
+++ Union/VONMenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VONMenuTableViewController.h
+//  Union
+//
+//  Created by Duri Abdurahman Duri on 8/2/14.
+//  Copyright (c) 2014 Duri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VONMenuTableViewController : UITableViewController
+
+@end
