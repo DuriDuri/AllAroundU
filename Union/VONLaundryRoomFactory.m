82ce16484c3513dd859c8fec45a331e90cf81e73
Duri <durid@union.edu>
2014-08-12 00:12:51 -0400

    Add Laundry Room Factory class

---
+++ Union/VONLaundryRoomFactory.m
@@ -0,0 +1,13 @@
+//
+//  VONLaundryRoomFactory.m
+//  Union
+//
+//  Created by Duri Abdurahman Duri on 8/11/14.
+//  Copyright (c) 2014 Duri. All rights reserved.
+//
+
+#import "VONLaundryRoomFactory.h"
+
+@implementation VONLaundryRoomFactory
+
+@end
