780defdcb48d1c7ba9f39c032ef5711ef1ec7c19
Duri <durid@union.edu>
2014-08-13 16:01:35 -0400

    Save when frozen

---
+++ Union/VONLaundryDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VONLaundryDetailViewController.h
+//  Union
+//
+//  Created by Duri Abdurahman Duri on 8/12/14.
+//  Copyright (c) 2014 Duri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VONLaundryDetailViewController : UIViewController
+
+@end
